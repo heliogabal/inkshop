b9f510a32ca55f556e5bbbef5c6228659e59be3c
heliogabal <rhalbmann@gmail.com>
2018-04-16 17:30:24 +0200

    set Matomo to attribute the Goal conversion to the first referrer used instead of the last one, https://matomo.org/faq/general/faq_106/#faq_106

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -70,6 +70,7 @@
 <script type="text/javascript">
   var _paq = _paq || [];
   // tracker methods like "setCustomDimension" should be called before "trackPageView"
+  _paq.push(['setConversionAttributionFirstReferrer', true]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
